956c69be30b9d8c95027248742f750bffccf180b
victor20-tech <victoradomako10@gmail.com>
2025-10-08 17:41:31 +0000

    Fix: Update import path for global styles in _app.tsx

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,5 +1,5 @@
 import Layout from "@/components/layouts/Layout";
-import "../styles/globals.css";
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { CountProvider } from "@/context/CountContext";
 
